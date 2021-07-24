d286cd8ce02b60986a509c20026b1e895287e23e
Mayar Arafa <mayararafa94@gmail.com>
2021-07-24 02:46:02 +0000

    fastmri data curl

---
+++ fastmri_data/curl_knee_multicoil.sh
@@ -0,0 +1,4 @@
+#curl -C - "https://fastmri-dataset.s3.amazonaws.com/multicoil_train.tar.gz?AWSAccessKeyId=AKIAJM2LEZ67Y2JL3KRA&Signature=XdcG%2B4At%2BXRDRSEJ7BUrJrEiEeU%3D&Expires=1634098906" --output multicoil_train.tar.gz
+#curl -C - "https://fastmri-dataset.s3.amazonaws.com/multicoil_val.tar.gz?AWSAccessKeyId=AKIAJM2LEZ67Y2JL3KRA&Signature=HVPIbme69KbZxJU2g33Oi9XvO%2Fg%3D&Expires=1634098906" --output multicoil_val.tar.gz
+curl -C - "https://fastmri-dataset.s3.amazonaws.com/knee_multicoil_test_v2.tar.gz?AWSAccessKeyId=AKIAJM2LEZ67Y2JL3KRA&Signature=Non2VbghYzfPP8y30yluCF9H7fM%3D&Expires=1634098906" --output knee_multicoil_test_v2.tar.gz
+curl -C - "https://fastmri-dataset.s3.amazonaws.com/knee_multicoil_challenge.tar.gz?AWSAccessKeyId=AKIAJM2LEZ67Y2JL3KRA&Signature=wJSZhQ2lzxAaAeUeb6eSzuob1mc%3D&Expires=1634098906" --output knee_multicoil_challenge.tar.gz
